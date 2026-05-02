696608263c6abd5228f6731c62b82f749e6ae330
Tunaanhgamedev <161286441+Tunaanhgamedev@users.noreply.github.com>
2026-05-03 00:03:08 +0700

    TwoSum

---
+++ Js/Easy/1TwoSum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const prevMap = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const diff = target - nums[i];
+        if (prevMap.has(diff)) {
+            return [prevMap.get(diff), i];
+        }
+        prevMap.set(nums[i], i);
+    }
+};
